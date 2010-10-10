538ebf889597cb788b5678e3b3270fdfe74fd3a7
Andrew Cameron <amcameron@gmail.com>
2010-10-10 16:39:11 -0400

    perform tasks for step 3 using existing functions

--- part3.m
+++ part3.m
@@ -2,7 +2,10 @@
 % Depends on Octave 3.2 and the "image" package,
 % available here: http://octave.sourceforge.net/image/overview.html
 
-% open files
+close all;
+
+% open files in grayscale
+% TODO: acquire cameraman.tif and tire.tif
 lena = rgb2gray(imread('lena.tif'));
 %cman = rgb2gray(imread('cameraman.tif'));
 
@@ -11,17 +14,31 @@ l_small = imresize(lena, 1/4, 'linear');
 %c_small = imresize(cman, 1/4, 'linear');
 
 % plot down-sampled images
-figure
-imshow(l_small);
-%figure
-%imshow(c_small);
+figure; imshow(l_small); title('Lena (downsampled)');
+%figure; imshow(c_small); title('Cameraman (downsampled)');
 
-% perform digital zooming using NN, bilinear, and bicubic interpolations
-% TODO: this
+% perform digital zooming using NN, bilinear, and bicubic interpolations;
+% be sure to turn off the antialiasing filter
+l_nn = imresize(l_small, 4, 'nearest', 0);
+l_bilin = imresize(l_small, 4, 'linear', 0);
+l_bicub = imresize(l_small, 4, 'cubic', 0);
+%c_nn = imresize(c_small, 4, 'nearest', 0);
+%c_bilin = imresize(c_small, 4, 'linear', 0);
+%c_bicub = imresize(c_small, 4, 'cubic', 0);
 
 % plot up-sampled images
-% TODO: this
+figure; imshow(l_nn); title('Nearest Neighbour');
+figure; imshow(l_bilin); title('Bilinear');
+figure; imshow(l_bicub); title('Bicubic');
+%figure; imshow(c_nn); title('Nearest Neighbour');
+%figure; imshow(c_bilin); title('Bilinear');
+%figure; imshow(c_bicub); title('Bicubic');
 
 % compute PSNR between original images and each corresponding up-sampled
-% image
-% TODO: this
+% image, and display the calculated values
+psnr_l_nn = PSNR(lena, l_nn)
+psnr_l_bilin = PSNR(lena, l_bilin)
+psnr_l_bicub = PSNR(lena, l_bicub)
+%psnr_c_nn = PSNR(cman, c_nn)
+%psnr_c_bilin = PSNR(cman, c_bilin)
+%psnr_c_bicub = PSNR(cman, c_bicub)
