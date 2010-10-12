39433a6fb2dd34e6420cd8cd5ac1db46e5819c10
Andrew Cameron <amcameron@gmail.com>
2010-10-11 20:54:16 -0400

    add implementation for part 5

---
+++ part5.m
@@ -0,0 +1,32 @@
+#!/usr/bin/octave3.2
+% Depends on Octave 3.2 and the "image" package,
+% available here: http://octave.sourceforge.net/image/overview.html
+
+close all;
+
+% Create test image and Fourier transform; plot them.
+f = zeros(256);
+f(:, 108:148) = 1;
+F = fftshift(fft2(f));
+figure; imshow(f);
+figure; imshow(abs(F), []);
+
+% Rotate image; plot it and its Fourier transform.
+frot = imrotate(f, 45, 'nearest', 'crop', 0);
+Frot = fftshift(fft2(frot));
+figure; imshow(frot);
+figure; imshow(abs(Frot), []);
+
+% Open lena.tif and find both components of its Fourier transform.
+lena = rgb2gray(imread('lena.tif'));
+Flena = fft2(lena);
+Flena_mag = abs(Flena);
+Flena_phs = angle(Flena);
+
+% Perform inverse Fourier transforms on the magnitude and phase separately.
+% TODO: why are these abs() calls necessary?
+lena_mag = abs(ifft2(Flena_mag));
+lena_phs = abs(ifft2(Flena_phs));
+figure; imshow(lena);
+figure; imshow(lena_mag);
+figure; imshow(lena_phs);
