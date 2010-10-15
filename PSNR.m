80ba293ce39ed8d404c13c3c1b3281d964e9bcb3
Andrew Cameron <amcameron@gmail.com>
2010-10-15 08:11:47 -0400

    fix possible precision error in PSNR
    also, simplify assert statement to use size() instead of rows() and
    columns()

--- PSNR.m
+++ PSNR.m
@@ -3,5 +3,5 @@ function result = PSNR(f, g)
 	result = 10*log10(255*255/MSE(f,g));
 
 function result = MSE(f, g)
-	assert( rows(f)==rows(g) && columns(f)==columns(g) );
-	result = sum(sum( (f-g) .^ 2 )) / ( rows(f)*columns(f) );
+	assert(size(f)==size(g));
+	result = double(sum(sum( (f-g) .^ 2 ))) / ( rows(f)*columns(f) );
